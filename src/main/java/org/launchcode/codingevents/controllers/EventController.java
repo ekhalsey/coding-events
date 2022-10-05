d9353a32d5ace4e8ad11cf23afe2d682ca6e38ac
EmmaHalsey <ekhalsey@yahoo.com>
2022-10-05 14:05:08 -0600

    thymeleaf ch 13 exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,15 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/events")
 public class EventController {
-    private static List<String> events = new ArrayList<>();
+    private static Map<String,String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model ) {
+//        model.addAttribute("events", events);
+//        return "events/index";
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -28,8 +35,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, String eventDescription) {
+        events.put(eventName, eventDescription);
         return "redirect:";
     }
 
