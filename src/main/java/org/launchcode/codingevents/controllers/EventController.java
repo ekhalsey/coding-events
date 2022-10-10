a40619730e0f2a42dbd914ba945ef4c85bbb79b0
EmmaHalsey <ekhalsey@yahoo.com>
2022-10-10 09:07:37 -0600

    made model folder and modified templates to display Event classes instead of strings

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,17 +16,18 @@ import java.util.Map;
 @Controller
 @RequestMapping("/events")
 public class EventController {
-    private static Map<String,String> events = new HashMap<>();
+    //private static Map<String,String> events = new HashMap<>();
+    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model ) {
-//        model.addAttribute("events", events);
-//        return "events/index";
-        events.put("Menteaship","A fun meetup for connecting with mentors");
-        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
-        events.put("Javascripty", "An imaginary meetup for Javascript developers");
         model.addAttribute("events", events);
         return "events/index";
+//        events.put("Menteaship","A fun meetup for connecting with mentors");
+//        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+//        events.put("Javascripty", "An imaginary meetup for Javascript developers");
+//        model.addAttribute("events", events);
+//        return "events/index";
     }
 
     //lives at /events/create
@@ -35,8 +37,9 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, String eventDescription) {
-        events.put(eventName, eventDescription);
+    public String createEvent(@RequestParam String eventName,
+                              @RequestParam String eventDescription) {
+        events.add(new Event(eventName, eventDescription));
         return "redirect:";
     }
 
