9c9a2a9f485e711d5bfd02c9cba7774f67cafee3
EmmaHalsey <ekhalsey@yahoo.com>
2022-10-15 17:28:58 -0500

    added enum class and drop down in create template

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,6 +2,7 @@ package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -33,6 +34,7 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(Model model) {
         model.addAttribute("event", new Event());
+        model.addAttribute("types", EventType.values());
         return "events/create";
     }
 
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -24,28 +24,33 @@ public class Event {
     @Positive(message="Number of attendees must be one or more.")
     private int numberOfAttendees;
 
+    private EventType type;
+
     private int id;
     private static int nextId = 1;
 
-    public Event(){};
-    public Event(String name, String description, String contactEmail) {
+    public Event(){
+        this.id = nextId;
+        nextId++;
+    };
+
+    public Event(String name, String description, String contactEmail, String location, boolean registrationRequired, int numberOfAttendees, EventType type) {
+        this();
         this.name = name;
         this.description = description;
-        this.id = nextId;
         this.contactEmail = contactEmail;
-        nextId++;
+        this.location = location;
+        this.registrationRequired = registrationRequired;
+        this.numberOfAttendees = numberOfAttendees;
+        this.type = type;
     }
 
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) {this.name = name;}
 
-    public String getContactEmail() {
-        return contactEmail;
-    }
+    public String getContactEmail() {return contactEmail;}
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
     }
@@ -57,32 +62,20 @@ public class Event {
         this.description = description;
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() {return id;}
 
-    public String getLocation() {
-        return location;
-    }
-    public void setLocation(String location) {
-        this.location = location;
-    }
+    public String getLocation() {return location;}
+    public void setLocation(String location) {this.location = location;}
 
-    public boolean isRegistrationRequired() {
-        return registrationRequired;
-    }
+    public boolean isRegistrationRequired() {return registrationRequired;}
+    public void setRegistrationRequired(boolean registrationRequired) {this.registrationRequired = registrationRequired;}
 
-    public void setRegistrationRequired(boolean registrationRequired) {
-        this.registrationRequired = registrationRequired;
-    }
+    public int getNumberOfAttendees() {return numberOfAttendees;}
+    public void setNumberOfAttendees(int numberOfAttendees) {this.numberOfAttendees = numberOfAttendees;}
 
-    public int getNumberOfAttendees() {
-        return numberOfAttendees;
-    }
+    public EventType getType() {return type;}
 
-    public void setNumberOfAttendees(int numberOfAttendees) {
-        this.numberOfAttendees = numberOfAttendees;
-    }
+    public void setType(EventType type) {this.type = type;}
 
     @Override
     public String toString() {
